70dbe76cc801a2448a1d3dedd055266968a1bb5a
霈政 邱 <paizanmay@gmail.com>
2018-10-20 21:41:14 +0800

    no message

--- api/models.py
+++ api/models.py
@@ -4,9 +4,9 @@ from enum import Enum
 class Item(models.Model):
 
     class ItemStatus(Enum):
-        want = 0
-        want_and_have = 1
-        recommend = 2
+        want = "0"
+        want_and_have = "1"
+        recommend = "2"
 
     name = models.TextField()
-    status = models.IntegerField(choices=[i.value for i in ItemStatus])
+    status = models.CharField(max_length=2, choices=[(i.name, i.value) for i in ItemStatus])
