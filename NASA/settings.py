f3aa9a5aa6e04290f71e15f6b5d7134753ab8fea
霈政 邱 <paizanmay@gmail.com>
2018-10-21 00:00:07 +0800

    Add mysql

--- NASA/settings.py
+++ NASA/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'NASA.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'nasa',
+        'USER': 'nasa',
+        'PASSWORD': 'nasahackathon',
+        'HOST': 'nasa-hackathon-db.cnzoglpssp0b.ap-northeast-1.rds.amazonaws.com',
+        'PORT': '3306',
     }
 }
 
