c58ef5038386a01e98d5615916d2c2c773493e7f
霈政 邱 <paizanmay@gmail.com>
2018-10-21 07:56:21 +0800

    no message

--- api/models.py
+++ api/models.py
@@ -1,6 +1,16 @@
 from django.db import models
 from enum import Enum
 
+
+class User(models.Model):
+    male_number = models.IntegerField()
+    female_number = models.IntegerField()
+    children_number = models.IntegerField()
+    baby_number = models.IntegerField()
+    old_number = models.IntegerField()
+    disease = models.TextField()
+
+
 class Item(models.Model):
 
     class ItemStatus(Enum):
@@ -10,3 +20,4 @@ class Item(models.Model):
 
     name = models.TextField()
     status = models.CharField(max_length=2, choices=[(i.value, i.name) for i in ItemStatus])
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, related_name="item")
